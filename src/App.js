7483c8876b95071741fcfdff610771a57ab4c2d3
Adarsh <adarshraj0210@gmail.com>
2021-08-05 12:39:57 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Switch>
+      <Route exact path='/'>
+          This is home page.
+      </Route>
+      <Route exact path='/started'>
+          started.
+      </Route>
+      <Route >
+          This is 404.
+      </Route>
+
+    </Switch>
+    
   );
 }
 
