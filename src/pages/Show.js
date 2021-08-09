25908bebc1e3df732d7f4cc51ef52d8e2b411641
Adarsh <adarshraj0210@gmail.com>
2021-08-09 16:44:03 +0530

    styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,6 +5,7 @@ import ShowMainData from "../show/ShowMainData";
 import Details from "../show/Details";
 import Seasons from "../show/Seasons";
 import Cast from "../show/Cast";
+import { InfoBlock, ShowPageWrapper } from "./ShowStyled";
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -62,7 +63,7 @@ const Show = () => {
     return <div>Error Occured: {error}</div>;
   }
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -71,25 +72,25 @@ const Show = () => {
         tags={show.genres}
       />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
